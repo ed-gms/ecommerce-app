27c5f53c2b68d28f9fd53d741a356f7682cf3ccd
ed-silva <32685663+ed-silva@users.noreply.github.com>
2018-07-18 14:32:26 -0700

    adding state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.png';
 import './App.css';
+import Card from './components/Card';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toggleLogo: true,
+      cards: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }],
+    };
+  }
+
+  toggleLogo = () => {
+    this.setState(prevState => ({
+      toggleLogo: !prevState.toggleLogo,
+    }));
+  };
+
   render() {
+    const { toggleLogo, cards } = this.state;
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img
+            src={logo}
+            className={toggleLogo ? 'static-logo' : 'animated-logo'}
+            alt="logo"
+            onClick={this.toggleLogo}
+          />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <div className="Grid">{cards.map(card => <Card duration={150} key={card.id} />)}</div>
       </div>
     );
   }
---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Card.css';
+
+const Card = () => (
+  <div className="card">
+    <img src="juice.jpg" alt="Vitamin Juice" className="card-image" />
+    <div className="container">
+      <h3>
+        Vitamin Juice <span className="price">$25.00</span>
+      </h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate suscipit deleniti sequi
+        ea laborum ullam ut dolore, ratione reprehenderit pariatur aliquam sed molestiae obcaecati
+        accusamus debitis magnam mollitia quod officiis?
+      </p>
+    </div>
+  </div>
+);
+
+export default Card;
